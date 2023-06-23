b01ef99b28a733d158e7b1559dcf97722d6340b6
KambaraReo <r.kambara1997@gmail.com>
2023-06-23 18:03:46 +0900

    本番環境に手動デプロイできることを確認するため，ファイル例を作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # DNSリバインディング攻撃からの保護機能が導入されているため，許可するホストを自分で設定
+  config.hosts << "oshidoko-rk-829fae334cbd.herokuapp.com"
 end
