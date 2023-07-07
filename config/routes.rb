640709a40395012bd4cbd843ccb1ee7b771221ca
KambaraReo <r.kambara1997@gmail.com>
2023-07-07 08:40:21 +0000

    [update]ルーティングの記述を整理

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   get 'samples/index'
   root 'homes#index'
   get 'homes/index'
-  get 'users/edit_password'
-  patch 'users/update_password'
   devise_for :users,
     controllers: { registrations: 'users/registrations' }
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    collection do
+      get 'edit_password'
+      patch 'update_password'
+    end
+  end
 end
