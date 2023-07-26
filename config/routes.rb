3df7b6ac9b0e312e7d6c3b2cbca5e6ad65a168a3
KambaraReo <r.kambara1997@gmail.com>
2023-07-26 06:18:42 +0000

    [update]postsコントローラーをusersコントローラーにネストさせるよう，ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       patch 'update_password'
       resources :profiles, only: [:edit, :update]
       get 'confirm_withdrawal'
+      resources :posts, only: [:new, :create]
     end
   end
-  resources :posts, only: [:new, :create]
 end
