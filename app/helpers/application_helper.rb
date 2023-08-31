b3d6f3022268403a8432fbdb3812ae158c93e157
KambaraReo <r.kambara1997@gmail.com>
2023-08-31 02:42:04 +0000

    [update]ページタイトルを表示するための記述を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  BASE_TITLE = "OSHIDOKO - 日向坂46の聖地マップ".freeze
+
+  def full_title(page_title)
+    page_title.blank? ? BASE_TITLE : "#{page_title} | #{BASE_TITLE}"
+  end
 end
