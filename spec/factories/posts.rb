1ce261b0628e3f5e99c13acf0fdb29677fd855c1
KambaraReo <r.kambara1997@gmail.com>
2023-09-08 04:44:59 +0000

    [add]postモデルを対象としたmodel specを実装

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :post do
-    name { "" }
-    address { "" }
-    latitude { "" }
-    longitude { "" }
-    description { "MyString" }
+    title { "東京駅" }
+    address { "〒100-0005 東京都千代田区丸の内１丁目" }
+    latitude { "35.681382" }
+    longitude { "139.76608399999998" }
+
+    user
   end
 end
