bc178c37345464e30c3913ad82d9fddaf1fa8d50
KambaraReo <r.kambara1997@gmail.com>
2023-09-08 06:19:06 +0000

    [add]お問い合わせにおけるActionMailer機能のrspecを実装

--- spec/mailers/previews/contact_mailer_preview.rb
+++ spec/mailers/previews/contact_mailer_preview.rb
@@ -1,3 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
+  def contact_mail
+    contact = Contact.new(
+      name: "tester",
+      email: "tester@example.com",
+      content: "お問い合わせです。"
+    )
+
+    ContactMailer.contact_mail(contact)
+  end
 end
